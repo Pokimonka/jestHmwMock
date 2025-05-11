635c2f5397def49f4a7435cce6104a1e40625ccd
Pokimonka <alenkaponomareva7@gmail.com>
2025-05-11 12:42:59 +0300

    add try-catch

--- src/getLevel.js
+++ src/getLevel.js
@@ -2,12 +2,16 @@
 import fetchData from './http';
 
 export default function getLevel(userId) {
+  try {
   const response = fetchData(`https://server/user/${userId}`);
-  
-  // TODO: логика обработки
   if (response.status === 'ok') {
      return `Ваш текущий уровень: ${response.level}`; 
   }
   
   return `Информация об уровне временно недоступна`;
+  } catch {
+    return `Информация об уровне временно недоступна`;
+
+  }
+
 }
